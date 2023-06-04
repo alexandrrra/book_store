dd1efc3746813adc2ea524ca74e5d317068ae98b
Alexandr-Gusev <34835129+Alexandr-Gusev@users.noreply.github.com>
2023-06-04 07:29:16 +0300

    Merge fix

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
     faUser, faHeart, faShoppingCart, faSearch, faIdCard, faSignOut,
-    faPersonWalkingArrowRight, faPen, faBook
+    faPersonWalkingArrowRight, faPen, faBook, faTrashCan, faPlus, faMinus
 } from '@fortawesome/free-solid-svg-icons';
 
 import router from './router'
 import store from "./store";
 import PrimeVue from 'primevue/config';
 import VueCookies from 'vue-cookies'
+import ToastService from 'primevue/toastservice';
 
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -24,10 +25,11 @@ import InputText from "primevue/inputtext"
 import Button from "primevue/button"
 import Password from 'primevue/password';
 import Dialog from 'primevue/dialog';
+import Toast from 'primevue/toast';
 
 library.add(
   faUser, faHeart, faShoppingCart, faSearch, faIdCard, faSignOut,
-  faPersonWalkingArrowRight, faPen, faBook
+  faPersonWalkingArrowRight, faPen, faBook, faTrashCan, faPlus, faMinus
 );
 
 const app = createApp(App);
@@ -36,6 +38,7 @@ app.use(router);
 app.use(store);
 app.use(PrimeVue, { ripple: true });
 app.use(VueCookies, { expires: '365d' })
+app.use(ToastService);
 
 app.directive('ripple', Ripple);
 app.directive('badge', BadgeDirective);
@@ -45,5 +48,6 @@ app.component('InputText', InputText);
 app.component('Button', Button);
 app.component('Password', Password);
 app.component('Dialog', Dialog);
+app.component('Toast', Toast);
 
 app.mount('#app');
